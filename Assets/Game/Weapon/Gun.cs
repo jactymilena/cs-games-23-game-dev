abd843d12477f1a4b1c84064a6aa2ad6dfa89405
CS Games <participant@laboratoire.uqac.ca>
2023-03-18 11:18:26 -0400

    coded a bullet and reloading mechanic

--- Assets/Game/Weapon/Gun.cs
+++ Assets/Game/Weapon/Gun.cs
@@ -17,10 +17,14 @@ public class Gun : Weapon
     [SerializeField] private float _bulletForce = 2f;
     public Rigidbody Bullet;
     [SerializeField] private Transform _bulletSpawn;
-    
+    [SerializeField] private int _magSizeMax = 30;
+    [SerializeField] private int _magSize;
+    private bool HasAmmunition => _magSize > 0;
+
     private void Awake()
     {
         _currentCooldown = _totalCooldownTime;
+        _magSize = _magSizeMax;
     }
 
     // Update is called once per frame
@@ -44,18 +48,42 @@ public class Gun : Weapon
                 Fire();
             }
         }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+            StartCoroutine(ReloadCoroutine());
+
 
         _currentCooldown -= Time.deltaTime;
     }
     
     public void Fire()
     {
-        
-        var bulletInstance = 
-            Instantiate(Bullet, _bulletSpawn.position, _bulletSpawn.rotation);
+        if (HasAmmunition){
+            var bulletInstance =
+                Instantiate(Bullet, _bulletSpawn.position, _bulletSpawn.rotation);
 
-        bulletInstance.AddForce(transform.forward * _bulletForce, ForceMode.VelocityChange);
+            bulletInstance.AddForce(transform.forward * _bulletForce, ForceMode.VelocityChange);
+            --_magSize;
+        }
+        else 
+            StartCoroutine(ReloadCoroutine());
+        
         // OnGunShoot.Invoke();
+    }
+    
+    // Reloading coroutine
+    IEnumerator ReloadCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Reloading");
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Reloading done ");
+
+        _magSize = _magSizeMax;
     }
 }
 
